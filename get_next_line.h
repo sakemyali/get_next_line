9572802310391c0175c9139e1ecdb3511d4f7a5e
Moulay Ali Sakurai El Idrissi <mosakura@c2r2s3.42tokyo.jp>
2025-11-01 18:14:37 +0900

    init commit

---
+++ get_next_line.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mosakura <mosakura@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/01 16:17:53 by mosakura          #+#    #+#             */
+/*   Updated: 2025/11/01 16:22:06 by mosakura         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h> //malloc, free
+# include <unistd.h> //read
+
+#endif
